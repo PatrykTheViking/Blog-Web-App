de024e53ed45dbec8be9b7133d387af4bb1e8707
Patryk <patryk.bytow@wp.pl>
2020-09-08 15:01:09 +0200

    Registration form and topic protection added

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -9,6 +10,7 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     """Display all topics"""
     topics = Topic.objects.order_by('date_added')
@@ -16,6 +18,7 @@ def topics(request):
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required()
 def topic(request, topic_id):
     """Display single topic and all attached posts"""
     topic = Topic.objects.get(id=topic_id)
@@ -25,6 +28,7 @@ def topic(request, topic_id):
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required()
 def new_topic(request):
     """Add new topic"""
     if request.method != 'POST':
@@ -42,6 +46,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required()
 def new_entry(request, topic_id):
     """Add new entry for a certain topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -63,6 +68,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required()
 def edit_entry(request, entry_id):
     """Edit existing entry"""
     entry = Entry.objects.get(id=entry_id)
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 
 urlpatterns = [
     # include default authentication urls
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    # registration site
+    path('register/', views.register, name='register'),
+
 ]
