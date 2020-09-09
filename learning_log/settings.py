15047ff85063a925f6e540ecb14536b18c1bef37
Patryk <patryk.bytow@wp.pl>
2020-09-09 13:48:06 +0200

    Project ready for upload

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +33,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 LOGIN_URL = 'users:login'
+
+# Heroku definition
+django_heroku.settings(locals())
 
 INSTALLED_APPS = [
     'learning_logs',
